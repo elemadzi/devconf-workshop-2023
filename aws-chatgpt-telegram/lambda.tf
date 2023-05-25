ade4673a7e3f0138d93551b961e904ac24ae46b0
livhuwanim <livhuwani.madzivhandila@momentum.co.za>
2023-05-25 13:56:52 +0200

    chartmsg

--- aws-chatgpt-telegram/lambda.tf
+++ aws-chatgpt-telegram/lambda.tf
@@ -13,9 +13,18 @@ data "archive_file" "dummy" {
 }
 
 resource "aws_lambda_function" "chatgpt" {
-
+  function_name = var.function_name
+  filename      = data.archive_file.dummy.output_path
+  description   = "Process interaction between ChatGPT and Telegram."
+  handler       = "lambda_function.message_handler"
+  runtime       = "python3.10"
+  reserved_concurrent_executions = 5
+  role        = aws_iam_role.lambda_exec_role.arn
+  memory_size = 128
+  timeout     = 300
 }
 
 resource "aws_lambda_function_url" "chatgpt" {
-
+  function_name      = aws_lambda_function.chatgpt.function_name
+  authorization_type = "NONE"
 }
