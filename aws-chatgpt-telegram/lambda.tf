a1d2215f0dfd820c78e4f83c72b0e95bb2f17ea1
livhuwanim <livhuwani.madzivhandila@momentum.co.za>
2023-05-25 12:44:50 +0200

    chartmsg

--- aws-chatgpt-telegram/lambda.tf
+++ aws-chatgpt-telegram/lambda.tf
@@ -3,14 +3,19 @@
 # AWS Lambda Resources
 ##
 
-# data "archive_file" "dummy" {
+data "archive_file" "dummy" {
+  type        = "zip"
+  output_path = "${path.module}/lambda_dummy_package.zip"
+  source {
+    content  = "hello"
+    filename = "dummy.text"
+  }
+}
 
-# }
+resource "aws_lambda_function" "chatgpt" {
 
-# resource "aws_lambda_function" "chatgpt" {
+}
 
-# }
+resource "aws_lambda_function_url" "chatgpt" {
 
-# resource "aws_lambda_function_url" "chatgpt" {
-
-# }
+}
